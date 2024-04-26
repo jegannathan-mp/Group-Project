29309358872b446830d2a28e09d58c9b82b21e4e
jegannathan-mp <mpjegannathanmech@gmail.com>
2024-04-26 14:06:53 +0530

    iniofne

---
+++ Music_player/script.js
@@ -0,0 +1,8 @@
+
+const content = document.querySelector(".cont-2");
+const Playimage = document.querySelector(".play-img");
+const musicName = document.querySelector(".head-title");
+const musicArtist = document.querySelector(".art-name");
+const Audio = document.querySelector(".main-song");
+const prevBtn = document.querySelector("#backward");
+const nextBtn = document.querySelector("#forward");
